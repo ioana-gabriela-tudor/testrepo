28a9da5396cc7b7fa4f96cffeb8a1d794eaf8c9a
Ioana-Gabriela Tudor <ioana.tudor@moduscreate.com>
2024-11-26 08:01:00 -0500

    practice task

--- FollowersCount/followers_count.js
+++ FollowersCount/followers_count.js
@@ -11,9 +11,17 @@ function checkCountValue() {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
+  } else if (count === 0){
+    alert("Followers count has been reset");
   }
 }
 
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
+
+function resetCount() {
+    count = 0; //Reset value to 0
+    displayCount(); //Display the count
+    checkCountValue(); // Check count value and display messages
 }
