7d9b6bbbe0c1f32716cdca28b851d07ff78276ae
Ioana-Gabriela Tudor <ioana.tudor@moduscreate.com>
2024-11-27 09:55:31 -0500

    practice task

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,13 +3,26 @@ function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let result;
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-        let result = multiply(num1, num2);
+        result = multiply(num1, num2);
 
         // Display the result
-        displayResult(result);
+        displayResult(result,'multiplication');
+
+        // Perform the operation
+        result = add(num1, num2);
+
+        // Display the result
+        displayResult(result, 'addition');
+
+        // Perform the operation
+        result = division(num1, num2);
+
+        // Display the result
+        displayResult(result, 'division');
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -23,8 +36,24 @@ function multiply(a, b) {
     return a * b;
 }
 
-function displayResult(result) {
+function add(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Add the numbers
+    return a + b;
+}
+
+function division(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Divide the numbers
+    return a / b;
+}
+
+function displayResult(result, operation) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.textContent = `The result for ${operation} is: ${result}`;
 }
